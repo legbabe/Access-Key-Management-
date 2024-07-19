da598f40208ebc8ddbde7d6d788529d2ad3057de
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-07-19 02:58:00 +0000

    vercel config updated

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 console.log(__dirname)
 
 //adding static middleware
-app.use(express.static('public'));
+app.use(express.static(__dirname +'/public'));
 app.use(express.json());
 app.use(cookieParser())
 
